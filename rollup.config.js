8004f7e70d024acbc618964746cf32a4fe96e042
Maximilian Schulke <hello@maximilianschulke.com>
2021-04-14 13:33:03 +0200

    add css support

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,43 @@
+import path from 'path';
+
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import postcss from 'rollup-plugin-postcss';
 import { terser } from 'rollup-plugin-terser';
 
 
 const production = !process.env.ROLLUP_WATCH;
 
-export default {
-	input: 'client/js/main.js',
-	output: {
-		file: 'static/js/main.js',
-		format: 'iife',
-		sourcemap: true
+export default [
+	{
+		input: 'client/js/main.js',
+		output: {
+			file: 'static/js/main.js',
+			format: 'iife',
+			sourcemap: true
+		},
+		plugins: [
+			resolve(),
+			commonjs(),
+			production && terser()
+		]
 	},
-	plugins: [
-		resolve(),
-		commonjs(),
-		production && terser()
-	]
-};
+	{
+		input: 'client/css/main.css',
+		output: {
+			dir: 'static/css',
+			exports: 'auto',
+			format: 'cjs'
+		},
+		plugins: [
+			postcss({
+				config: {
+					path: "./postcss.config.js",
+				},
+				extensions: [".css"],
+				extract: 'main.css',
+				minimize: production
+			})
+		]
+	}
+];
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	purge: {
+		enabled: true,
+		content: ['./templates/**/*.html'],
+	},
+	theme: {
+		extend: {}
+	},
+	variants: {},
+	plugins: []
+};
