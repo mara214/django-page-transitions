3a6d4e01fd9ddb44d8ca299572a5bb1958842c10
Maximilian Schulke <hello@maximilianschulke.com>
2021-04-15 10:43:25 +0200

    add typescript support

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
-import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import postcss from 'rollup-plugin-postcss';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
 
 
@@ -8,7 +9,7 @@ const production = !process.env.ROLLUP_WATCH;
 
 export default [
 	{
-		input: 'client/js/main.js',
+		input: 'client/js/main.ts',
 		output: {
 			file: 'static/js/main.js',
 			format: 'iife',
@@ -17,6 +18,7 @@ export default [
 		plugins: [
 			resolve(),
 			commonjs(),
+			typescript(),
 			production && terser()
 		]
 	},
